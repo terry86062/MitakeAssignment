87c46f9256c7ed4af49eb1ed346299767e45fed6
Terry <terry86062@gmail.com>
2019-06-07 18:48:17 +0800

    Extension - UIView with func ma_FromNib

---
+++ MitakeAssignment/Extension/UIView_extension.swift
@@ -0,0 +1,21 @@
+//
+//  UIView_extension.swift
+//  MitakeAssignment
+//
+//  Created by 黃偉勛 Terry on 2019/6/7.
+//  Copyright © 2019 Terry. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func ma_FromNib<T: UIView>() -> T {
+        
+        guard let view = Bundle.main.loadNibNamed(String(describing: T.self), owner: nil, options: nil)?[0] as? T else { return T() }
+        
+        return view
+        
+    }
+    
+}
---
+++ MitakeAssignment/View/Chart/LineChartView.swift
@@ -0,0 +1,15 @@
+//
+//  LineChartView.swift
+//  MitakeAssignment
+//
+//  Created by 黃偉勛 Terry on 2019/6/7.
+//  Copyright © 2019 Terry. All rights reserved.
+//
+
+import UIKit
+
+class LineChartView: UIView {
+    
+    
+    
+}
